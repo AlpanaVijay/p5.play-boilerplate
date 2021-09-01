7ad80071889d72c3e1a2d18d210ccb092124a7fb
AlpanaVijay <70261445+AlpanaVijay@users.noreply.github.com>
2021-09-01 14:31:16 +0530

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -1,9 +1,30 @@
+var movingRect, fixedRect;
 function setup() {
   createCanvas(800,400);
-  createSprite(400, 200, 50, 50);
+  movingRect = createSprite(400, 200, 50, 50);
+  fixedRect = createSprite(200,100,50,80)
+
+  movingRect.shapeColor = "green";
+  fixedRect.shapeColor = "green"
 }
 
 function draw() {
-  background(255,255,255);  
+  background("black");  
+
+  movingRect.x = mouseX;
+  movingRect.y = mouseY;
+
+  console.log("for difference " +movingRect.x - fixedRect.x);
+  console.log("addtiion of width "+movingRect.width/2 + fixedRect.width/2);
+
+  if(movingRect.x - fixedRect.x < movingRect.width/2 + fixedRect.width/2){
+    movingRect.shapeColor = "red";
+    fixedRect.shapeColor = "red"
+  }
+
+  else{
+    movingRect.shapeColor = "green";
+    fixedRect.shapeColor = "green"
+  }
   drawSprites();
 }
